2b8a4681c7547fb88acea880ccf8ac53bc81ec04
Rick Mark <rickmark@outlook.com>
2024-02-16 17:38:46 -0800

    Initial Commit

---
+++ read_ean/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  read_ean
+//
+//  Created by Rick Mark on 2/16/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
